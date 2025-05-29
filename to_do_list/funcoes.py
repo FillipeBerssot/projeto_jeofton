b62cf2b4036ee6f4dc13c72243733e954dc83b63
T-chillz <thiagogdionisio@hotmail.com>
2025-05-29 17:38:20 -0300

    Adição de função de encerramento do programa

---
+++ to_do_list/funcoes.py
@@ -0,0 +1,5 @@
+#Função de encerrar o programa:
+def encerrarPrograma():
+    print('\nEncerrando o programa. Obrigado por usar o nosso sistema de Lista de Afazeres!')
+    print('======================================================================================')
+    exit()
