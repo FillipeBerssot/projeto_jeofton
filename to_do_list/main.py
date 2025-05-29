a903ca7f3c103c2a796ed5c68b51b0fc641cf925
fillipeberssot95@hotmail.com <fillipeberssot95@hotmail.com>
2025-05-29 16:54:31 -0300

    finalizado a criação do menu

---
+++ to_do_list/main.py
@@ -0,0 +1,32 @@
+
+
+def menu():
+    while True:
+        opcao = input('''
+                      ===============================
+                      .:PROJETO: LISTA DE AFAZERES:.
+                      ===============================
+                      MENU:
+                      [1] - .:ADICIONAR TAREFAS:.
+                      [2] - .:LISTAR TAREFAS:.
+                      [3] - .:ATUALIZAR TAREFAS:.
+                      [4] - .:DELETAR TAREFA:.
+                      [5] - .:ENCERRAR PROGRAMA:.
+                      
+                      ESCOLHA SUA OPÇÃO: ''').strip()
+        
+        if opcao == '1':
+            ...
+        elif opcao == '2':
+            ...
+        elif opcao == '3':
+            ...
+        elif opcao == '4':
+            ...
+        elif opcao == '5':
+            ...
+        else:
+            print('OPS! Opção inválida! Tente Novamente.')
+
+if __name__ == '__main__':
+    menu()
